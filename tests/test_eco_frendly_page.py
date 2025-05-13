55ca081447659baccea6c5fb63c76d5e76c6e339
Aliaksandr_Tarfimau_2 <trafalex91@gmail.com>
2025-05-13 07:35:53 +0300

    'mark_update'

--- tests/test_eco_frendly_page.py
+++ tests/test_eco_frendly_page.py
@@ -1,15 +1,19 @@
+import pytest
+
+
+@pytest.mark.smoke
 def test_select_option(category_page):
     category_page.open_page()
     category_page.select_subcategory()
     category_page.verify_image_is_displayed()
 
-
+@pytest.mark.regression
 def test_select_sort_price(category_page):
     category_page.open_page()
     category_page.sort_products_by_price()
     category_page.verify_selected_sorting_option()
 
-
+@pytest.mark.extended
 def test_check_training_option(category_page):
     category_page.open_page()
     category_page.select_training_element()
--- tests/test_sail_page.py
+++ tests/test_sail_page.py
@@ -1,13 +1,21 @@
+import pytest
+
+
+@pytest.mark.extended
 def test_click_women_sale_image(sale_page):
     sale_page.open_page()
     sale_page.click_women_category()
     sale_page.verify_page_title("Women Sale")
 
+
+@pytest.mark.regression
 def test_click_men_sale_image(sale_page):
     sale_page.open_page()
     sale_page.click_men_category()
     sale_page.verify_page_title("Men Sale")
 
+
+@pytest.mark.smoke
 def test_training_video_displayed(sale_page):
     sale_page.open_page()
     sale_page.hover_on_training_menu()
